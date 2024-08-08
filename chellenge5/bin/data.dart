d4ad415ebec0247676af9bdb59a44f6f1794a7f9
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-08-08 14:56:44 +0300

    done

---
+++ chellenge5/bin/data.dart
@@ -0,0 +1,19 @@
+Map<String, dynamic> data = {
+  "students": [
+    {
+      "id": "S001",
+      "name": "Amina",
+      "grades": [85.5, 90.0, 78.0]
+    },
+    {
+      "id": "S002",
+      "name": "Khalid",
+      "grades": [92.0, 88.5, 79.5]
+    },
+    {
+      "id": "S003",
+      "name": "Layla",
+      "grades": [70.0, 65.0, 80.0]
+    }
+  ]
+};
